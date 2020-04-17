a708b1ae42c4938fbfea3cba79a267377da67dc9
alexeytra <alekceytr@mail.ru>
2020-04-17 12:56:56 +0800

    committ

---
+++ dbschema.js
@@ -0,0 +1,23 @@
+let db = {
+    users: [
+        {
+            userId: 'kljflkjsfj',
+            email: 'user@mail.com',
+            handle: 'user',
+            createdAt: '2020-04-06T05:53:49.374Z',
+            imageUrl: 'url',
+            bio: 'Hello',
+            website: 'dlfkld',
+            location: 'London'
+        }
+    ],
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is something new',
+            createdAt: '2020-04-04T08:09:10.274Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
